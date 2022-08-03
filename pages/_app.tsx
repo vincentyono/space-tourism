67ff392f0650d8d96a866f79a27d75ac751f89a7
Vincent Yono <vincentyono@protonmail.com>
2022-08-03 13:05:24 +0700

    Project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import React from "react";
+
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Frontend Mentor | Space tourism website</title>
+        <meta
+          name="description"
+          content="Space tourism website is one of Frontend Mentor Challenge"
+        />
+        <link rel="icon" href="/static/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
