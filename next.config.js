8ad788cda4e2c664e0196515dc66868df1316bd0
Vincent Yono <vincentyono@protonmail.com>
2022-08-07 17:24:20 +0700

    edit some settings

--- next.config.js
+++ next.config.js
@@ -17,6 +17,14 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+
+  basePath: "/nextjs-pages",
+  assetPrefix: "nextjs-pages",
 };
 
 module.exports = nextConfig;
