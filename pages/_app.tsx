6d52b29448be489bd5829938c42cd2cbf3c6c151
Vincent Yono <vincentyono@protonmail.com>
2022-08-08 13:36:49 +0700

    add html lang='en' tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import React from "react";
-
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
+    <html lang="en">
       <Head>
         <title>Frontend Mentor | Space tourism website</title>
         <meta
@@ -16,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/static/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </React.Fragment>
+    </html>
   );
 }
 
